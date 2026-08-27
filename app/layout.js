712da3cc11c3980fa77772a88c6da93f369d3f53
sakibhossen18 <sakibhossen18@gmail.com>
2026-08-28 02:40:45 +0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Deye Load Shedding Monitor V6",
+  description: "Deye Cloud Load Shedding Monitor",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
